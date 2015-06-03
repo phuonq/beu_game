4a6df8104c89bf58139a3f9a1cd45ed0845b4c37
Johannes Truong Le <j.truongle@hotmail.com>
2015-06-03 15:39:50 +0200

    Uploaded Gamestatemanager inlc. Gamestate_start and Gamestate_game, Texture Manager

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
-#include <SFML/Graphics.hpp>
-#include <iostream>
+#include "Game.h"
+#include "Gamestatestart.h"
+#include "Gamestategame.h"
 
 int main()
 {
-	std::cout << "Hello" << std::endl;
-    return 0;
+
+
+	Game game;
+
+	game.push_state(new Gamestate_start(&game));
+	game.game_loop();
+
+	return 0;
 }
